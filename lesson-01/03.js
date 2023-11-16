e9dbfdae776912951fad435144a8f850a80a8126
HannaDov <dovgiallohanna@gmail.com>
2023-11-16 19:44:41 +0300

    Increment and decrement

---
+++ lesson-01/03.js
@@ -0,0 +1,25 @@
+// code
+/*
+​let number = 1;
+📋
+и используя лишь
+
+--number   number--  ++number   number++
+📋
+Получи в консоли 1,3,3,1
+
+
+
+❗Нужно вызвать консоль ЧЕТЫРЕ раза
+
+Пример:
+
+    console.log(number**);
+console.log(**number);
+console.log(number**);
+console.log(**number); */
+let number = 1
+console.log(number++);
+console.log(++number);
+console.log(number--);
+console.log(--number)
