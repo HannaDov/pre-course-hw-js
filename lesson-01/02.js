c460431c2b2b2e3950a5bb9085535fd7944b86fe
HannaDov <dovgiallohanna@gmail.com>
2023-11-16 18:12:36 +0300

    even and odd numbers

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -6,8 +6,8 @@ let number1 = Math.floor(Math.random() * 100)
  а затем проверь их выводя в консоль: «[вставь сюда полученное число] :Это четное число» или «[вставь сюда полученное число] :Это нечетное число».*/
 let number = Math.floor(Math.random() * 100)
 if (number % 2=== 0){
-    console.log(number + ":Это четное число")
+    console.log(number, ":Это четное число")
 }
 if(number % 2!== 0){
-    console.log(number + ":Это нечетное число")
+    console.log(number, ":Это нечетное число")
 }
