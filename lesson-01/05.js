d799e5233c24afdadb6b4bed5a58071a76c07812
HannaDov <dovgiallohanna@gmail.com>
2023-11-18 17:48:16 +0300

    switch case6
    # Conflicts:
    #	lesson-01/04.js

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -1,17 +1,16 @@
 // code
 let rule = "Еще не родился тот человек, который, поставил бы цель, и не смог бы стать программистом.";
 
-switch (rule.length) {
-    case 25:
+switch (true) {
+    case (rule.length===25):
         console.log("50 на 50");
         break;
     case rule.length < 25:
-        console.log("Все таки нет правил без исключения");
+        console.log("Что-то пошло не так");
         break;
     case rule.length > 25:
         console.log("У меня 100 пудов все получится");
         break;
     default:
-        console.log("Что-то пошло не так");
         break;
 }
