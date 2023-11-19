a960455c51741fcf6a75abb4ce685d75fbfb9bdf
HannaDov <dovgiallohanna@gmail.com>
2023-11-19 20:25:07 +0300

    less 2 copy object

---
+++ lesson-02/05.js
@@ -0,0 +1,14 @@
+// code
+
+let passport = {
+    name: "Petr",
+    surname: "Petrov",
+};
+
+let passportCopy={
+    name:'Ivan',
+    surname: "Petrov"
+}
+
+console.log(passport)
+console.log(passportCopy)
